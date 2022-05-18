f92bf4f0a5ed8083c04c7cd15ea217e6ff631412
DonBlaSeP <dvivenegas.25@gmail.com>
2022-05-18 16:42:25 -0400

    cards arregladas

--- js/btn-nav.js
+++ js/btn-nav.js
@@ -1,3 +1,3 @@
 $(".hide").on('click', function() {
     $("nav ul").slideToggle(600);
-  })
+})
--- js/btn-sidebar.js
+++ js/btn-sidebar.js
@@ -1,11 +1,11 @@
 $(".sidebar-btn").on('click', function() {
     $(".sidebar").toggle();
-    $(".sidebar-content").animate({width: 'toggle', display: 'block'}, 500);
+    $(".sidebar-content").animate({ width: 'toggle', display: 'block' }, 500);
     $(".sidebar-content a").css("display", "block");
-  })
+})
 
-  $(".close").on('click', function() {
+$(".close").on('click', function() {
     $(".sidebar").toggle();
-    $(".sidebar-content").animate({width: 'toggle'});
-    $(".sidebar-content a").animate({width: 'toggle'});
-  })
+    $(".sidebar-content").animate({ width: 'toggle' });
+    $(".sidebar-content a").animate({ width: 'toggle' });
+})
